228db636fd42ac88b6be7286bf1891ba48a9c4d1
Guillaume Smaha <guillaume.smaha@gmail.com>
2017-10-16 17:40:39 -0400

    Remove fail test

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -29,13 +29,16 @@ import org.junit.internal.AssumptionViolatedException;
 
 class Base {
 
-    protected void run() {
+   protected void runWithRandomFail() {
         double r = Math.random();
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
+    }
+
+   protected void run() {
     }
 
 }
